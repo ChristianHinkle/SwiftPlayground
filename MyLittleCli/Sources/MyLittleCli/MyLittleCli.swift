49b932d01f83986b005dc1d4f90eab48e8eb689c
Christian Hinkle <christianthinkle@gmail.com>
2026-06-18 09:44:42 -0600

    [swift] Make simple print statements based on args given by the CLI user.

--- MyLittleCli/Sources/MyLittleCli/MyLittleCli.swift
+++ MyLittleCli/Sources/MyLittleCli/MyLittleCli.swift
@@ -5,5 +5,23 @@
 struct MyLittleCli {
     static func main() {
         print("Hello, world!")
+
+        print()
+
+        // Note: As a CLI application, we ignore the first arg, as that is conventionally the program name, but it's not guarenteed to
+        // be anyway, and we don't need it anyway.
+
+        if (CommandLine.arguments.count <= 1) {
+            print("You gave me no args.")
+            return
+        }
+
+        print("I have your args:")
+        print()
+
+        // Note: No allocations here. `String` is copy-on-write, and `dropFirst` returns an `ArraySlice` which is simply a span/view.
+        for arg in CommandLine.arguments.dropFirst() {
+            print(arg)
+        }
     }
 }
