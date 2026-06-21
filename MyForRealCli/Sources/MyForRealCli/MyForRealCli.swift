37018eac69734389be1ee1836b21d9fc03ae6e31
Christian Hinkle <christianthinkle@gmail.com>
2026-06-20 23:59:41 -0600

    [swift][package] Add dependency to MyCommandDispatcher package and target for MyForRealCli package. And add a simple public function usage as a little text.

--- MyForRealCli/Sources/MyForRealCli/MyForRealCli.swift
+++ MyForRealCli/Sources/MyForRealCli/MyForRealCli.swift
@@ -1,9 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import MyCommandDispatcher
+
 @main
 struct MyForRealCli {
     static func main() {
         print("Hello, world!")
+
+        let heyy = MyYoFunc();
     }
 }
