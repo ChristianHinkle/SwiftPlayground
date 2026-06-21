a3cee8ec2d6454e8b234beb01f5a9ddb578535e3
Christian Hinkle <christianthinkle@gmail.com>
2026-06-20 23:23:26 -0600

    [swift][rename] Rename MyRealCli to MyForRealCli.

--- MyForRealCli/Package.swift
+++ MyForRealCli/Package.swift
@@ -4,16 +4,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyRealCli",
+    name: "MyForRealCli",
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
-            name: "MyRealCli"
+            name: "MyForRealCli"
         ),
         .testTarget(
-            name: "MyRealCliTests",
-            dependencies: ["MyRealCli"]
+            name: "MyForRealCliTests",
+            dependencies: ["MyForRealCli"]
         ),
     ],
     swiftLanguageModes: [.v6]
--- MyForRealCli/Sources/MyForRealCli/MyForRealCli.swift
+++ MyForRealCli/Sources/MyForRealCli/MyForRealCli.swift
@@ -2,7 +2,7 @@
 // https://docs.swift.org/swift-book
 
 @main
-struct MyRealCli {
+struct MyForRealCli {
     static func main() {
         print("Hello, world!")
     }
