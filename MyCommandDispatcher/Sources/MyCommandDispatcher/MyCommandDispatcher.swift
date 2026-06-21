38fc4c7693afb159b00564734fa0898cac0db222
Christian Hinkle <christianthinkle@gmail.com>
2026-06-21 14:36:00 -0600

    [swift] Make very simple "dispatchCommandToFunction" function which calls on a command function based on the arg strings passed in. Placeholder logic for now.

--- MyCommandDispatcher/Sources/MyCommandDispatcher/MyCommandDispatcher.swift
+++ MyCommandDispatcher/Sources/MyCommandDispatcher/MyCommandDispatcher.swift
@@ -1,6 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public func MyYoFunc() -> Int32 {
-    return 8;
+public func myYoFunc() -> Int32 {
+    return 8
+}
+
+// TODO: [todo] Change the data structure of the "functionTable" parameter, to support namespaced command names and arguments.
+public func dispatchCommandToFunction(args: ArraySlice<String>, functionTable: [String: @Sendable () -> Void]) {
+    if (args.isEmpty)
+    {
+        return
+    }
+
+    if let function = functionTable[args.first!] {
+        function()
+    }
 }
--- MyForRealCli/Sources/MyForRealCli/MyForRealCli.swift
+++ MyForRealCli/Sources/MyForRealCli/MyForRealCli.swift
@@ -3,11 +3,27 @@
 
 import MyCommandDispatcher
 
+func commandProjectCreate(path: String) {
+    print("Creating project at", path)
+}
+
+func commandProjectCreateNoParamsJustForExperimenting() {
+    commandProjectCreate(path: "This is a fake placeholder string")
+}
+
+// TODO: [todo] Change the data structure of the "functionTable" parameter, to support namespaced command names and arguments.
+let myFunctionTable: [String: @Sendable () -> Void] = [
+    "projectCreate":  commandProjectCreateNoParamsJustForExperimenting
+]
+
 @main
 struct MyForRealCli {
     static func main() {
         print("Hello, world!")
 
-        let heyy = MyYoFunc();
+        // Note: As a CLI application, we ignore the first arg, as that is conventionally the program name, but it's not guarenteed to
+        // be anyway, and we don't need it anyway.
+
+        dispatchCommandToFunction(args: CommandLine.arguments.dropFirst(), functionTable: myFunctionTable)
     }
 }
